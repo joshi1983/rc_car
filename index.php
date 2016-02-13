8b8fc13720f7ae54b566ae09c8b4d517177b1bb1
unknown <JGreig@Josh-PC.nextdimensioninc.com>
2016-02-13 14:54:55 -0500

    web: started a static part of a website for controlling a car

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Car controller</title>
+		<script type="text/javascript" src="js/knockout-3.4.0.debug.js"></script>
+		<link type="text/css" rel="stylesheet" href="css/styles.css">
+	</head>
+	<body>
+		<div class="video-display">
+		</div>
+		<div class="steering">
+		
+		</div>
+		<div class="drive">
+		
+		</div>
+	</body>
+</html>
