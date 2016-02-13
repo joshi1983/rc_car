a5452c0bc428eac39f869b0fe8a7e4621aea4941
unknown <JGreig@Josh-PC.nextdimensioninc.com>
2016-02-13 14:55:51 -0500

    db: added a database initialization script

---
+++ db/init.sql
@@ -0,0 +1,23 @@
+create table `car_control_state`
+(
+	`id` int primary key auto_increment,
+	`steering_value` float not null default 0,
+	`speed_value` float not null default 0,
+	`is_desired` tinyint(1) not null default 0
+);
+
+create table `user`
+(
+	`id` int primary key auto_increment,
+	`username` varchar(20) not null unique,
+	`password_hash` varchar(32) not null
+)
+
+insert into `car_control_state`
+(`id`, `steering_value`, `speed_value`, `is_desired`) values
+(1, 0, 0, 1),
+(2, 0, 0, 0);
+
+insert into `user`(`id`, `username`, `password_hash`) values
+(1, 'josh', '');
+
