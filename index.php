ba4dd9c332f91f3beb38632b2d4edf391f70b722
unknown <JGreig@Josh-PC.nextdimensioninc.com>
2016-03-18 18:08:58 -0400

    index.php: added requirejs, bootstrap and made use of Durandal

--- index.php
+++ index.php
@@ -2,17 +2,14 @@
 <html lang="en">
 	<head>
 		<title>Car controller</title>
-		<script type="text/javascript" src="js/knockout-3.4.0.debug.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<script type="text/javascript" src="js/lib/jquery/jquery-2.2.0.js"></script>
+		<script type="text/javascript" src="js/lib/knockout/knockout-3.4.0.debug.js"></script>
 		<link type="text/css" rel="stylesheet" href="css/styles.css">
+		<link type="text/css" rel="stylesheet" href="js/lib/bootstrap-3.3.6-dist/css/bootstrap.css">
 	</head>
 	<body>
-		<div class="video-display">
-		</div>
-		<div class="steering">
-		
-		</div>
-		<div class="drive">
-		
-		</div>
+		<div id="applicationHost"></div>
+        <script src="js/lib/require/require.js" data-main="app/main"></script>
 	</body>
 </html>
