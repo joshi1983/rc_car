82f0b40cf6038b94d6449c4a17fd193515fd8e53
jgreig <josh.greig@gmail.com>
2016-03-18 22:39:04 -0400

    android: removed AppBarLayout, content_main and added a VideoView and EditText element

--- android/CarController/app/src/main/java/rc_car/com/carcontroller/MainActivity.java
+++ android/CarController/app/src/main/java/rc_car/com/carcontroller/MainActivity.java
@@ -1,31 +1,67 @@
 package rc_car.com.carcontroller;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_VIDEO_CAPTURE = 1;
+    private VideoView mVideoView;
+    private boolean capturingVideo = false;
+    private FloatingActionButton toggleVideoButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        toggleVideoButton = (FloatingActionButton) findViewById(R.id.fab);
+        mVideoView = (VideoView)findViewById(R.id.video_view);
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onPrepared(MediaPlayer mp) {
+                mVideoView.start();
+                mp.setLooping(true);
+
             }
         });
+    }
+
+    private void videoStoppedCapturing() {
+        toggleVideoButton.setImageResource(android.R.drawable.ic_menu_camera);
+        capturingVideo = false;
+    }
+
+    public void toggleVideoCaptureClicked(View view) {
+        if (capturingVideo) {
+            videoStoppedCapturing();
+        } else {
+            capturingVideo = true;
+            toggleVideoButton.setImageResource(android.R.drawable.btn_minus);
+            Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            videoStoppedCapturing();
+            mVideoView.setVideoURI(videoUri);
+        }
     }
 
     @Override
