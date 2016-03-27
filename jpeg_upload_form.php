f7f867000f393f9b6aed8c07fcd75ff97a4adfa8
jgreig <josh.greig@gmail.com>
2016-03-27 16:03:54 -0400

    added jpeg_upload_form.php for manually testing the api's picture upload feature

---
+++ jpeg_upload_form.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+ <head>
+	<title>Picture Upload test form</title>
+ </head>
+ <body>
+	<form action="http://google.com/rc_car/api/saveCameraFrame" method="post" enctype="multipart/form-data">
+		<input type="file" name="frame">
+		<input type="submit" value="Submit">
+	</form>
+ </body>
+</html>
