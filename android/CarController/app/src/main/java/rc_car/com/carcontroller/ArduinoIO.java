664b7af4685c384654ce24b8513d60f501534bf1
jgreig <josh.greig@gmail.com>
2016-03-28 23:39:36 -0400

    android: AndroidIO: started new class that will be responsible for talking with the Arduino device

---
+++ android/CarController/app/src/main/java/rc_car/com/carcontroller/ArduinoIO.java
@@ -0,0 +1,17 @@
+package rc_car.com.carcontroller;
+
+/**
+ * Responsible for communicating with Android application through USB cables.
+ */
+public class ArduinoIO {
+
+    public void setSteeringValue(double value) {
+
+    }
+
+    public void setDriveValue(double value) {
+
+    }
+
+
+}
