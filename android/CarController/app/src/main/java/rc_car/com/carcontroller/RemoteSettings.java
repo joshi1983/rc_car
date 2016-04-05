11922d635c09e6727e3b326e0a6f86c13c5758fd
jgreig <josh.greig@gmail.com>
2016-04-04 22:30:59 -0400

    android: RemoteSettings: simplified code in slightly in readJsonFromUrl

--- android/CarController/app/src/main/java/rc_car/com/carcontroller/RemoteSettings.java
+++ android/CarController/app/src/main/java/rc_car/com/carcontroller/RemoteSettings.java
@@ -74,8 +74,7 @@ public class RemoteSettings {
         try {
             BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
             String jsonText = readAll(rd);
-            JSONObject json = new JSONObject(jsonText);
-            return json;
+            return new JSONObject(jsonText);
         } finally {
             is.close();
         }
