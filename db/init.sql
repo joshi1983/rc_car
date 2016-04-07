0cdeb6834a91de1750f682dec2ee90e2978057b2
jgreig <josh.greig@gmail.com>
2016-04-06 20:27:11 -0400

    db: init.sql: started new frame table and added last_changed field to a few tables

--- db/init.sql
+++ db/init.sql
@@ -3,7 +3,8 @@ create table `car_control_state`
 	`id` int primary key auto_increment,
 	`steering_value` float not null default 0,
 	`speed_value` float not null default 0,
-	`is_desired` tinyint(1) not null default 0
+	`is_desired` tinyint(1) not null default 0,
+	`latest_change` datetime
 );
 
 create table `user`
@@ -16,16 +17,24 @@ create table `user`
 create table `preference`
 (
 	`id` int primary key auto_increment,
-	`is_recording` tinyint(1) default 0
+	`is_recording` tinyint(1) default 0,
+	`latest_change` datetime
 );
 
-insert into `preference`(id, is_recording) values
-(1, 0);
+create table `frame`
+(
+	`id` int primary key auto_increment,
+	`is_recording` mediumblob, -- for jpeg data
+	`latest_change` datetime
+);
+
+insert into `preference`(`id`, `is_recording`, `latest_change`) values
+(1, 0, NOW());
 
 insert into `car_control_state`
-(`id`, `steering_value`, `speed_value`, `is_desired`) values
-(1, 0, 0, 1),
-(2, 0, 0, 0);
+(`id`, `steering_value`, `speed_value`, `is_desired`, `latest_change`) values
+(1, 0, 0, 1, NOW()),
+(2, 0, 0, 0, NOW());
 
 insert into `user`(`id`, `username`, `password_hash`) values
 (1, 'josh', '');
