9a4ce2c7da5cccf65d03d837cbfb489e6ffdf10a
jgreig <josh.greig@gmail.com>
2016-04-30 13:18:22 -0400

    android: CarState: added speedValue and steeringValue properties and a few methods

--- android/CarController/app/src/main/java/rc_car/com/carcontroller/CarState.java
+++ android/CarController/app/src/main/java/rc_car/com/carcontroller/CarState.java
@@ -1,7 +1,30 @@
 package rc_car.com.carcontroller;
 
 /**
- * Created by JGreig on 4/5/16.
+ * CarState is basically a data transfer object or a Java bean for car state.
+ *
+ * It may represent either desired state or the latest actual state echoed back from the Arduino device
  */
 public class CarState {
+    private double speedValue;
+    private double steeringValue;
+
+    public CarState(double steeringValue, double speedValue) {
+        this.steeringValue = steeringValue;
+        this.speedValue = speedValue;
+    }
+
+    public void copyFrom(CarState other) {
+        this.speedValue = other.speedValue;
+        this.steeringValue = other.steeringValue;
+    }
+
+    public double getSteeringValue() {
+        return steeringValue;
+    }
+
+    public double getSpeedValue() {
+        return speedValue;
+    }
+
 }
